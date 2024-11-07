5b74d95d5163c4a999d2fd42b8c4d86c212192f6
Rusty <johngiomilas@gmail.com>
2024-11-07 23:14:43 +0200

    Cleaned up the whole file, also added a diffirent configuration.

--- bot.py
+++ bot.py
@@ -1,42 +1,12 @@
 import discord
+from config import *
 from discord.ext import commands
-import time
-import os
-import json
-
-# CONFIG FILES // CONFIG.JSON
-if os.path.exists(os.getcwd() + "/config.json"):
-    with open("./config.json") as f:
-        configData = json.load(f);
-else:
-    configTemplate = {"Token": "", "Prefix": ""};
-    with open(os.getcwd() + "/config.json", "w+") as f:
-        json.dump(configTemplate, f);
-
-token = configData["Token"];
-prefix = configData["Prefix"];
 
 intents = discord.Intents.all();                                       
 bot = commands.Bot(command_prefix=prefix, intents=intents, help_command=None);
 
-color = discord.Color.dark_gold(); # ΕΔΩ ΒΑΣΕΤΕ ΤΟ ΧΡΩΜΑ ΣΤΟ EMBED ΣΑΣ
-servername = '' # ΕΔΩ ΒΑΖΕΤΕ ΤΟ ΟΝΟΜΑ ΤΟΥ ΣΕΡΒΕΡ ΣΑΣ
-imagelink = '' # ΕΔΩ ΒΑΖΕΤΕ ΤΟ LINK ΤΗΣ ΕΙΚΟΝΑΣ ΤΟΥ ΣΕΡΒΕΡ
-
-# ROLE IDS
-StaffRole =  # Εδώ βάζεις το role id του Staff
-EmsRole =  # Εδώ ΄βάζεις το role id του Ems
-PoliceRole =  # Εδώ ΄βάζεις το role id του Police
-
-# APPLICATION LINKS
-StaffLink = '' # Εδώ ΄βάζεις το application link (Google Documents) του Police
-EmsLink = '' # Εδώ ΄βάζεις το application link (Google Documents) του Ems
-PoliceLink = '' # Εδώ ΄βάζεις το application link (Google Documents) του Staff
-
 @bot.event  
 async def on_ready():
-    print('Thank you for using my bot, if you have any questions dm: Playboy®#2737')
-    time.sleep(3)
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Your Applications"));
     print(f"{bot.user} has logged in");
 
---
+++ config.py
@@ -0,0 +1,18 @@
+import discord
+
+token = ""
+prefix = ""
+
+color = discord.Color.dark_gold(); # ΕΔΩ ΒΑΣΕΤΕ ΤΟ ΧΡΩΜΑ ΣΤΟ EMBED ΣΑΣ
+servername = '' # ΕΔΩ ΒΑΖΕΤΕ ΤΟ ΟΝΟΜΑ ΤΟΥ ΣΕΡΒΕΡ ΣΑΣ
+imagelink = '' # ΕΔΩ ΒΑΖΕΤΕ ΤΟ LINK ΤΗΣ ΕΙΚΟΝΑΣ ΤΟΥ ΣΕΡΒΕΡ
+
+# ROLE IDS
+StaffRole = None # Εδώ βάζεις το role id του Staff
+EmsRole = None # Εδώ βάζεις το role id του Ems
+PoliceRole = None # Εδώ βάζεις το role id του Police
+
+# APPLICATION LINKS
+StaffLink = '' # Εδώ βάζεις το application link (Google Documents) του Police
+EmsLink = '' # Εδώ βάζεις το application link (Google Documents) του Ems
+PoliceLink = '' # Εδώ βάζεις το application link (Google Documents) του Staff
