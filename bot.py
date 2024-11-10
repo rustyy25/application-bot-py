5001b3f1f457739f5f91e52f111cb55294d86f6b
Rusty <johngiomilas@gmail.com>
2024-11-10 11:59:12 +0200

    Removed the semicolons and added a run.cmd file to start the bot from there

--- bot.py
+++ bot.py
@@ -2,21 +2,21 @@ import discord
 from config import *
 from discord.ext import commands
 
-intents = discord.Intents.all();                                       
-bot = commands.Bot(command_prefix=prefix, intents=intents, help_command=None);
+intents = discord.Intents.all()                                
+bot = commands.Bot(command_prefix=prefix, intents=intents, help_command=None)
 
 @bot.event  
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Your Applications"));
-    print(f"{bot.user} has logged in");
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Your Applications"))
+    print(f"{bot.user} has logged in")
 
 @bot.command()
 async def apps(ctx):
-    StaffEmbed = discord.Embed(description=f"[<@&{StaffRole}>]({StaffLink})", color=color);
-    EmsEmbed = discord.Embed(description=f"[<@&{EmsRole}>]({EmsLink})", color=color);
-    PoliceEmbed = discord.Embed(description=f"[<@&{PoliceRole}>]({PoliceLink})", color=color);
+    StaffEmbed = discord.Embed(description=f"[<@&{StaffRole}>]({StaffLink})", color=color)
+    EmsEmbed = discord.Embed(description=f"[<@&{EmsRole}>]({EmsLink})", color=color)
+    PoliceEmbed = discord.Embed(description=f"[<@&{PoliceRole}>]({PoliceLink})", color=color)
     embeds = [StaffEmbed, EmsEmbed, PoliceEmbed]
     for i in embeds:
-        await ctx.send(embed=i);
+        await ctx.send(embed=i)
 
 bot.run(token)
